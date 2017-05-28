46a324694ea38211291513b7976d46da9deb6d73
Home <bondarenko_elijah@mail.ru>
2017-05-29 02:16:06 +0600

    Implement dao layer

---
+++ src/main/java/com/home/ilya/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.home.ilya.repository;
+
+import com.home.ilya.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+}
